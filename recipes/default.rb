96e2aa5cefa89b513b250ff48efbede7f521ec36
jonathanhoskin <github@hosk.in>
2016-10-09 20:06:18 +1300

    Revert "Revert "Start transmission very late on boot""
    This reverts commit e1dddd5c46ef35f352b71d0439ce430991f2c05a.

--- recipes/default.rb
+++ recipes/default.rb
@@ -33,7 +33,7 @@ require 'transmission-simple'
 service 'transmission' do
   service_name 'transmission-daemon'
   supports restart: true, reload: true
-  priority 90
+  priority { 2 => [:start, 80], 3 => [:start, 80], 4 => [:start, 80], 5 => [:start, 80], 6 => [:stop, 20] }
   action :nothing
 end
 
@@ -98,6 +98,6 @@ end
 service 'transmission' do
   service_name 'transmission-daemon'
   supports restart: true, reload: true
-  priority 90
+  priority { 2 => [:start, 80], 3 => [:start, 80], 4 => [:start, 80], 5 => [:start, 80], 6 => [:stop, 20] }
   action [:enable, :start]
 end
